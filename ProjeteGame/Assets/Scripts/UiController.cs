2e98b82a1b83a5fd90cc2325e12115f8c6027fa3
Francisco Silvério <franciscogsilverio@gmail.com>
2021-09-22 11:54:32 -0300

    Adição do monólogo pós leitura da carta

--- ProjeteGame/Assets/Scripts/UiController.cs
+++ ProjeteGame/Assets/Scripts/UiController.cs
@@ -5,13 +5,27 @@ using UnityEngine.UI;
 
 public class UiController : MonoBehaviour
 {
-    public GameObject Image; 
+    public GameObject Image;
+    public GameObject Monolog;
     public bool trigger;
+    private bool imageEnabled = false;
 
     private void Update()
     {
-        if (trigger == true && Input.GetKeyDown(KeyCode.E)) Image.SetActive(true);
-        if (trigger == false) Image.SetActive(false);
+        if (trigger == true && Input.GetKeyDown(KeyCode.E)) {
+            Image.SetActive(true);
+            imageEnabled = true;
+        }
+
+        if (!trigger)
+        {
+            Image.SetActive(false);
+            if (imageEnabled)
+            {
+               Monolog.SetActive(true);
+               imageEnabled = false;
+            }
+        }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
